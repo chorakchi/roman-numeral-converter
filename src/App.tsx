d2f224cfb9719682b2cd6e313238da75277a1ad4
mochorak <mohammad.chorakchi@akelius.de>
2020-11-02 05:19:32 +0100

    integration with UI and calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { RomanNumerals } from "./utility";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState<string | number>("");
+  const [inputValue, setInputValue] = useState<string>("");
+  const [isInteger, setIsInteger] = useState<boolean>(false);
+  useEffect(() => {
+    let Action = isInteger ? RomanNumerals.toRoman : RomanNumerals.fromRoman;
+    setResult(Action(inputValue));
+  }, [inputValue, isInteger]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>Roman Numerals converter (auto detect)</h3>
+      <h6>type your number and see the result below, only "IVXLCDM" and Number acceptable</h6>
+        <input
+        className="App-input"
+          placeholder="type here your number ..."
+          value={inputValue}
+          onChange={(e) =>
+            RomanNumerals.validator(e.target.value, setIsInteger, setInputValue)
+          }
+        />
+        <p>{result}</p>
       </header>
     </div>
   );
